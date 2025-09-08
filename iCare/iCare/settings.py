1e762f2c9e1e5311b995cb533611f0a7f69de299
hamadbah <hamad.coding@gmail.com>
2025-09-08 19:28:04 +0300

    Initial Commit for iCare Project

--- iCare/iCare/settings.py
+++ iCare/iCare/settings.py
@@ -66,8 +66,11 @@ WSGI_APPLICATION = 'iCare.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD':os.getenv('DATABASE_PASSWORD'),
+        'PORT':os.getenv('DATABASE_PORT')
     }
 }
 
